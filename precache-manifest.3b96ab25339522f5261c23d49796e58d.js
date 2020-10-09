551e2799b1671ba9595ebfc61401ef97fb5cd14b
STihn <you.lucky@bk.ru>
2020-10-09 03:42:13 +0300

    Updates

---
+++ precache-manifest.3b96ab25339522f5261c23d49796e58d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c8507fc3c7be98a9321bbe007b990f",
+    "url": "/weather-for-7-days/index.html"
+  },
+  {
+    "revision": "6c621c543af9a595c4a1",
+    "url": "/weather-for-7-days/static/css/main.ad5474aa.chunk.css"
+  },
+  {
+    "revision": "d65b53014f973472c958",
+    "url": "/weather-for-7-days/static/js/2.5eacdcf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-for-7-days/static/js/2.5eacdcf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c621c543af9a595c4a1",
+    "url": "/weather-for-7-days/static/js/main.b85e98ad.chunk.js"
+  },
+  {
+    "revision": "13e47d3d563fb68d2a4e",
+    "url": "/weather-for-7-days/static/js/runtime-main.51141093.js"
+  }
+]);
